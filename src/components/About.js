7709932a7ec4218aea5a12df47a089a979d89ef4
Danny Phillips <danielphillips0101@gmail.com>
2022-03-05 01:13:19 -0800

    updated about me and attempted to fix footer.

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,20 @@ function About() {
                 <Col> 
                     <Image src={Portrait} alt="Portrait" fluid></Image>
                 </Col>
-                <Col>
-                    <h2 className='text-center text-white'>About Me</h2>
-                    <p className='text-center text-white'>This is a short bio that I have to figure out how to write about myself so that employers can get to know me a little bit.  Longest run on sentence ever just before this.</p>
+                <Col className="text-center">
+                    <h2 className='text-white'>About Me</h2>
+                    <p className='fs-3 text-center text-white'>
+                        Hey there! I am Daniel (Danny) Phillips and I am a web developer based in Washington.
+                    </p>
+                    <br></br>
+                    <p className='text-center text-white'>
+                        I am an 11-year United States Navy veteran.  I worked in the nuclear power field for all 11 years, I chose to leave the Navy in 2020.  After some soul searching I chose to pursue a career in programming and web development. 
+                        I enrolled in the University of Washington's Full Stack Web Developer Bootcamp and I've been having a blast learning and coding since!  In my free time I love to work on many various forms of art and have been getting really
+                        into digital, so I love adding some custom graphic design elements into my work. 
+                        <br></br>
+                        <br></br>
+                        When I am not home working on coding or art, I love being in the mountains and forests.  Camping, backpacking, and hiking are some of my favorite activities.
+                    </p>
                 </Col>
             </Row>
         </Container>
--- src/components/Footerish.js
+++ src/components/Footerish.js
@@ -1,21 +1,34 @@
 import React from "react";
-import { Navbar, Nav, Row, Col } from "react-bootstrap";
+import { Navbar, Nav, Row, Col, Container } from "react-bootstrap";
 import {BsGithub, BsLinkedin} from "react-icons/bs"
+
+const footer = {
+    position: 'absolute',
+    left:0,
+    bottom:0,
+    right:0,
+};
 
 function Footerish() {
     return (
-        <Navbar bg="dark" expand="lg" variant="dark" fixed="bottom">
-            <Nav className="mx-auto">                    
-                <Row>
-                    <Col>
-                        <Nav.Link className="my-auto" href="https://github.com/DanielGPhillips"><BsGithub color="white" size="4rem"/></Nav.Link>
-                    </Col>
-                    <Col>
-                        <Nav.Link className="my-auto" href="https://linkedin.com/daniel-phillips-6b01381b0"><BsLinkedin color="white" size="4rem"/></Nav.Link>
-                    </Col>
-                </Row>                   
-            </Nav>
-        </Navbar>       
+        <Container style={footer}>
+            <footer>
+                <Container>
+                    <Navbar bg="dark" expand="lg" variant="dark">
+                        <Nav className="mx-auto">                    
+                            <Row>
+                                <Col>
+                                    <Nav.Link className="my-auto" href="https://github.com/DanielGPhillips"><BsGithub color="white" size="4rem"/></Nav.Link>
+                                </Col>
+                                <Col>
+                                    <Nav.Link className="my-auto" href="https://linkedin.com/daniel-phillips-6b01381b0"><BsLinkedin color="white" size="4rem"/></Nav.Link>
+                                </Col>
+                            </Row>                   
+                        </Nav>
+                    </Navbar>
+                </Container>
+            </footer>
+        </Container>       
     )
 }
 
